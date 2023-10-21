2a9260669b6b2b48ae49604a8801697d4becd16c
omarrachid <omar98rachid@gmail.com>
2023-10-21 18:52:07 +0200

    fix: build and serve and added vite esnext

--- vite-remote/vite.config.ts
+++ vite-remote/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      federation({
+        name: "remote_app",
+        filename: "remoteEntry.js",
+        exposes: {
+          './button' : './src/button'
+        },
+        shared: ["react", "react-dom"]
+      })
+  ],
+    build: {
+      target: "esnext", //browsers can handle the latest ES features
+    }
 })
